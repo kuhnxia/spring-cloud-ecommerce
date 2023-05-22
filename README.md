bd1ee54fe331ed8bd4926045a3f6e649d222252d
Kun Xia <kn991666@dal.ca>
2023-05-22 15:47:23 +0800

    initial

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# spring-cloud-e-commerce
+# spring-cloud-ecommerce
---
+++ payment-service/src/test/java/org/kun/springcloudecommerce/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kun.springcloudecommerce.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
