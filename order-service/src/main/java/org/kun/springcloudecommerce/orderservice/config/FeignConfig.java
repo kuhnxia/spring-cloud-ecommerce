38c42b70a7b4280e77af777d49b8a4a82bd02607
Kun Xia <kn991666@dal.ca>
2023-05-26 11:11:37 +0800

    implemented order service exception handler

--- order-service/src/main/java/org/kun/springcloudecommerce/orderservice/config/FeignConfig.java
+++ order-service/src/main/java/org/kun/springcloudecommerce/orderservice/config/FeignConfig.java
@@ -3,7 +3,9 @@ package org.kun.springcloudecommerce.orderservice.config;
 import feign.codec.ErrorDecoder;
 import org.kun.springcloudecommerce.orderservice.external.decoder.CustomErrorDecoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class FeignConfig {
     @Bean
     ErrorDecoder errorDecoder(){
