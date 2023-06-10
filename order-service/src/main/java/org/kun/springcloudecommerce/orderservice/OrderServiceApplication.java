febbad6f65a75bc3516d0a700adca4e45a0cb663
Kun Xia <kn991666@dal.ca>
2023-06-10 22:58:17 +0800

    Bug found! fallback method without return value.

--- order-service/src/main/java/org/kun/springcloudecommerce/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/kun/springcloudecommerce/orderservice/OrderServiceApplication.java
@@ -1,7 +1,11 @@
 package org.kun.springcloudecommerce.orderservice;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
--- order-service/src/main/java/org/kun/springcloudecommerce/orderservice/external/client/ProductService.java
+++ order-service/src/main/java/org/kun/springcloudecommerce/orderservice/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package org.kun.springcloudecommerce.orderservice.external.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.kun.springcloudecommerce.orderservice.exception.CustomException;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
@@ -13,4 +16,9 @@ public interface ProductService {
             @PathVariable("id") long productId,
             @RequestParam long quantity
     );
+
+    default ResponseEntity<Long> fallback(Exception e){
+        throw new CustomException("Product Service is not available", "UNAVAILABLE",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
