3cc73dd6389ff105a7bf2c13a838a8660b725061
Kun Xia <kn991666@dal.ca>
2023-06-11 21:38:57 +0800

    added Okta Security.

---
+++ cloud-gateway/src/main/java/org/kun/springcloudecommerce/cloudgateway/security/OktaOAuth2WebSecurity.java
@@ -0,0 +1,24 @@
+package org.kun.springcloudecommerce.cloudgateway.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OktaOAuth2WebSecurity {
+
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity){
+        httpSecurity
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return httpSecurity.build();
+    }
+}
