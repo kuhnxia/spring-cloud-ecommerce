d877b61677e6df0432f73c5f53f86503cf00bf9d
Kun Xia <kn991666@dal.ca>
2023-06-27 21:10:29 +0800

    A test with Mocking

--- order-service/src/test/java/org/kun/springcloudecommerce/orderservice/service/OrderServiceImplTest.java
+++ order-service/src/test/java/org/kun/springcloudecommerce/orderservice/service/OrderServiceImplTest.java
@@ -2,16 +2,27 @@ package org.kun.springcloudecommerce.orderservice.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.kun.springcloudecommerce.orderservice.entity.Order;
 import org.kun.springcloudecommerce.orderservice.external.client.PaymentService;
 import org.kun.springcloudecommerce.orderservice.external.client.ProductService;
+import org.kun.springcloudecommerce.orderservice.external.response.PaymentResponse;
+import org.kun.springcloudecommerce.orderservice.external.response.ProductResponse;
+import org.kun.springcloudecommerce.orderservice.model.PaymentMode;
 import org.kun.springcloudecommerce.orderservice.repository.OrderRepository;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
-class OrderServiceImplTest {
+public class OrderServiceImplTest {
     @Mock
     private OrderRepository orderRepository;
 
@@ -31,7 +42,52 @@ class OrderServiceImplTest {
     @DisplayName("Get Order - Success Scenario")
     @Test
     void test_When_Order_Success() {
+        Order order = getMockOrder();
+        when(orderRepository.findById(anyLong()))
+                .thenReturn(Optional.of(order));
 
+        when(restTemplate.getForObject(
+                "http://PRODUCT-SERVICE/product/"+ order.getProductId(),
+                ProductResponse.class
+        )).thenReturn(getMockProductResponse());
+
+        when(restTemplate.getForObject(
+                "http://PAYMENT-SERVICE/payment/order/" + order.getId(),
+                PaymentResponse.class
+        )).thenReturn(getMockPaymentResponse());
+
+
+    }
+
+    private PaymentResponse getMockPaymentResponse() {
+        return PaymentResponse.builder()
+                .paymentId(1)
+                .paymentDate(Instant.now())
+                .paymentMode(PaymentMode.CASH)
+                .amount(200)
+                .orderId(1)
+                .status("ACCEPTED")
+                .build();
+    }
+
+    private ProductResponse getMockProductResponse() {
+        return ProductResponse.builder()
+                .productId(2)
+                .name("iPhone")
+                .price(100)
+                .quantity(200)
+                .build();
+    }
+
+    private Order getMockOrder() {
+        return  Order.builder()
+                .orderDate(Instant.now())
+                .orderStatus("PLACED")
+                .id(1)
+                .amount(100)
+                .quantity(200)
+                .productId(2)
+                .build();
     }
 
 }
