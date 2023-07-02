9460c80a32e1f0ddaa550cef0d2d700d0430d80c
Kun Xia <kn991666@dal.ca>
2023-07-02 21:59:24 +0800

    added dependencies in OrderControllerTest class

--- order-service/src/test/java/org/kun/springcloudecommerce/orderservice/controller/OrderControllerTest.java
+++ order-service/src/test/java/org/kun/springcloudecommerce/orderservice/controller/OrderControllerTest.java
@@ -1,15 +1,42 @@
 package org.kun.springcloudecommerce.orderservice.controller;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.kun.springcloudecommerce.orderservice.OrderServiceConfig;
+import org.kun.springcloudecommerce.orderservice.repository.OrderRepository;
+import org.kun.springcloudecommerce.orderservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest({"server.port=0"})
 @EnableConfigurationProperties
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = OrderServiceConfig.class)
 public class OrderControllerTest {
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private MockMvc mockMvc;
 
+    static WireMockExtension wireMockExtension =
+            WireMockExtension.newInstance()
+                    .options(WireMockConfiguration
+                            .wireMockConfig()
+                            .port(8080))
+                    .build();
+
+    private ObjectMapper objectMapper =
+            new ObjectMapper()
+                    .findAndRegisterModules()
+                    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 }
