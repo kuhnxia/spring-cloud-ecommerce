89700c50ee4b8a544f147cee1bacbfecf24ceb74
Kun Xia <kn991666@dal.ca>
2023-07-03 17:15:37 +0800

    implemented set up methods

--- order-service/src/test/java/org/kun/springcloudecommerce/orderservice/controller/OrderControllerTest.java
+++ order-service/src/test/java/org/kun/springcloudecommerce/orderservice/controller/OrderControllerTest.java
@@ -3,8 +3,11 @@ package org.kun.springcloudecommerce.orderservice.controller;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.platform.commons.util.StringUtils;
 import org.kun.springcloudecommerce.orderservice.OrderServiceConfig;
 import org.kun.springcloudecommerce.orderservice.repository.OrderRepository;
 import org.kun.springcloudecommerce.orderservice.service.OrderService;
@@ -12,8 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 @SpringBootTest({"server.port=0"})
 @EnableConfigurationProperties
@@ -39,4 +48,57 @@ public class OrderControllerTest {
                     .findAndRegisterModules()
                     .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
                     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    @BeforeEach
+    void setup() throws IOException {
+        getProductDetailsResponse();
+        doPayment();
+        getPaymentDetails();
+        reduceQuantity();
+    }
+
+    private void reduceQuantity() {
+        wireMockExtension.stubFor(WireMock.put(WireMock.urlMatching("/product/reduceQuantity/.*"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+    }
+
+    private void getPaymentDetails() throws IOException {
+        wireMockExtension.stubFor(WireMock.get(WireMock.urlMatching("/payment/.*"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(
+                                StreamUtils.copyToString(
+                                        OrderControllerTest.class
+                                                .getClassLoader()
+                                                .getResourceAsStream("mock/GetPayment.json"),
+                                        Charset.defaultCharset()
+                                )
+                        )));
+    }
+
+    private void doPayment() {
+        wireMockExtension.stubFor(WireMock.post(WireMock.urlEqualTo("/payment"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+    }
+
+    private void getProductDetailsResponse() throws IOException {
+        wireMockExtension.stubFor(
+                WireMock.get("/product/1")
+                        .willReturn(
+                                WireMock.aResponse()
+                                        .withStatus(HttpStatus.OK.value())
+                                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                        .withBody(
+                                                StreamUtils.copyToString(
+                                                       OrderControllerTest.class
+                                                               .getClassLoader()
+                                                               .getResourceAsStream("mock/GetProduct.json"),
+                                                       Charset.defaultCharset()
+                                                ))));
+    }
 }
